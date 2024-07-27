577b61d35625d2418f635730ff0a1c435cbc32ec
Aswad Akram <142088695+aswadakram@users.noreply.github.com>
2024-07-27 20:21:29 +0500

    Update calculate_factorial.py

--- calculate_factorial.py
+++ calculate_factorial.py
@@ -1,7 +1,7 @@
 user_num = int(input("Enter a positive integer: "))
 
 if user_num < 0:
-    print("Please enter a positive integer.")
+    print("Invalid integar! Enter a positive integer.")
 else:
     result = 1
     for i in range(1, user_num + 1):
