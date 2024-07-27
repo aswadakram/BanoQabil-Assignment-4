27eff73c784cd25e360c790ed89a9d48e3e434b7
Aswad Akram <142088695+aswadakram@users.noreply.github.com>
2024-07-27 20:22:07 +0500

    Update calculate_factorial.py

--- calculate_factorial.py
+++ calculate_factorial.py
@@ -3,7 +3,7 @@ user_num = int(input("Enter a positive integer: "))
 if user_num < 0:
     print("Invalid integar! Enter a positive integer.")
 else:
-    result = 1
+    factorial = 1
     for i in range(1, user_num + 1):
-        result *= i
-    print("The factorial of", user_num, "is", result)
+        factorial *= i
+    print("The factorial of", user_num, "is", factorial)
