4d7c56cbbf4043b2ba9db1d5ce31f9a471f32ef6
Aswad Akram <142088695+aswadakram@users.noreply.github.com>
2024-07-27 20:19:06 +0500

    Add files via upload

---
+++ calculate_factorial.py
@@ -0,0 +1,9 @@
+user_num = int(input("Enter a positive integer: "))
+
+if user_num < 0:
+    print("Please enter a positive integer.")
+else:
+    result = 1
+    for i in range(1, user_num + 1):
+        result *= i
+    print("The factorial of", user_num, "is", result)
